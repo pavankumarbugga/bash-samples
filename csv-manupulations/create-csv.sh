3b77491ad60ee48715df2a9c7c6437f36a64f4fd
pavankumarbugga <pavankumar.bugga@gmail.com>
2022-04-03 11:58:42 -0500

    support multiple comma separeted values in a column

--- csv-manupulations/create-csv.sh
+++ csv-manupulations/create-csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "first_name,last_name,age" > names.csv; \
-echo "Jim,Bugga,21" >> names.csv
-echo "Rachel,Crook,23" >> names.csv
-echo "Katey,Patrick,42" >> names.csv
-echo "Zackie,Williams,47" >> names.csv
+echo "first_name,last_name,age,hobbies" > names.csv; \
+echo "Jim,Bugga,21,Badminton" >> names.csv
+echo "Rachel,Crook,23,\"swimming,painting\"" >> names.csv
+echo "Katey,Patrick,42,gardening" >> names.csv
+echo "Zackie,Williams,47,\"swimming,golf\"" >> names.csv
